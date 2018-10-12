ab7ecb295fc392a006ed59687bf4a3b13d7d782c
nidagewcc <nidagewcc@163.com>
2018-10-12 16:30:22 +0800

    漏了个mapper

---
+++ src/main/java/com/phoenix/travel/dao/ScenicMapper.java
@@ -0,0 +1,36 @@
+package com.phoenix.travel.dao;
+
+import com.phoenix.travel.po.Scenic;
+import com.phoenix.travel.po.ScenicExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ScenicMapper {
+    int countByExample(ScenicExample example);
+
+    int deleteByExample(ScenicExample example);
+
+    int deleteByPrimaryKey(Integer scenicId);
+
+    int insert(Scenic record);
+
+    int insertSelective(Scenic record);
+
+    List<Scenic> selectByExampleWithBLOBs(ScenicExample example);
+
+    List<Scenic> selectByExample(ScenicExample example);
+
+    Scenic selectByPrimaryKey(Integer scenicId);
+
+    int updateByExampleSelective(@Param("record") Scenic record, @Param("example") ScenicExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") Scenic record, @Param("example") ScenicExample example);
+
+    int updateByExample(@Param("record") Scenic record, @Param("example") ScenicExample example);
+
+    int updateByPrimaryKeySelective(Scenic record);
+
+    int updateByPrimaryKeyWithBLOBs(Scenic record);
+
+    int updateByPrimaryKey(Scenic record);
+}
