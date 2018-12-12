2a1679fe3c7cca03c966ac273b3cdff4465969fe
nidagewcc <nidagewcc@163.com>
2018-12-12 15:00:05 +0800

    update something

--- src/main/java/com/phoenix/travel/common/exception/TravelBizRuntimeException.java
+++ src/main/java/com/phoenix/travel/common/exception/TravelBizRuntimeException.java
@@ -7,6 +7,8 @@ package com.phoenix.travel.common.exception;
  * @create 2018/10/10
  */
 public class TravelBizRuntimeException extends RuntimeException {
+    private static final long serialVersionUID = -6763486995463561690L;
+
     public TravelBizRuntimeException() {
         super();
     }
